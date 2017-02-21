eea129391ef7a5d867509112bb3fa8ffa4d85154
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2017-02-21 13:27:44 -0600

    added load functionality

--- Game.py
+++ Game.py
@@ -7,18 +7,24 @@ def greeting():
     savedata["Name"] = player_name
     choose_prof(savedata)
 
-
-f = open('savefile', 'r')
-read_file = f.read()
-f.close()
-savedata = ast.literal_eval(read_file)
-print(savedata)
-greeting()
-print(savedata)
-sds = str(savedata)
-nf = open('savefile', 'w')
-nf.write(sds)
-nf.close()
+savedata = {}
+print("What would you like to do? \n>> 1. New Game\n>> 2. Load Game")
+menu_choice = input()
+if menu_choice == "1":
+    greeting()
+    sds = str(savedata)
+    nf = open('savefile_'+savedata["Name"], 'w')
+    nf.write(sds)
+    nf.close()
+elif menu_choice == "2":
+    load_choice = input("Which character do you want to load?")
+    f = open('savefile_'+load_choice, 'r')
+    read_file = f.read()
+    f.close()
+    savedata = ast.literal_eval(read_file)
+    print(savedata)
+else:
+    print("Invalid choice")
 enemy = choose_enemy()
 begin_fight(savedata, enemy)
 
